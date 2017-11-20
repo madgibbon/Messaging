f12ffe758478eb54a8dfaf56d22efe45dbb3bde6
Richard Hill <richardhill101@gmail.com>
2017-11-20 13:38:42 +0000

    First commit

---
+++ Messaging/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Messaging
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DoSubscribe();
+        }
+        public void OnClick(object o, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+        public void DoSubscribe()
+        {
+            MessagingCenter.Subscribe<MainPage>(this,"Change",(sender)=> { Label1.Text = "Goodbye"; });
+        }
+    }
+}
---
+++ Messaging/Page2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Messaging
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        public void OnClicked(object o, EventArgs e)
+        {
+            var mainPage = new MainPage();
+            MessagingCenter.Send<MainPage>(mainPage, "Changed");
+            Navigation.PopAsync();
+        }
+    }
+}
